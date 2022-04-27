fda980f39c6935944b0cece3081ef140423a223b
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-04-26 22:40:15 -0300

    doc: change files to folder trab1

---
+++ trab1/math.js
@@ -0,0 +1,19 @@
+const sum = (a,b) =>{
+    return a+b
+}
+
+const multi = (a,b) =>{
+    return a*b
+}
+
+const div = (a,b) =>{
+    return a/b
+}
+
+const subt = (a,b) =>{
+    return a-b
+}
+
+module.exports = {
+    sum,multi,div,subt
+}
